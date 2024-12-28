ace3df1f489564e39301b88bb78d6951b7c246db
Rambabu Mallavarapu <Rambabu.Mallavarapu@in.ey.com>
2024-12-28 22:54:55 +0530

    added code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports:[ReactiveFormsModule]
+  imports:[ReactiveFormsModule],
+
 })
 export class LoginComponent implements OnInit {
 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: LoginService) { }
   profileForm = new FormGroup({
     username: new FormControl('',Validators.required),
     password: new FormControl('',Validators.required),
   });
   sendrequest() {
-    if(this.profileForm.controls['username'].value =='ram' && this.profileForm.controls['password'].value =='ram'){
-     this.router.navigate(['/user']);
-    }
+     this.service.GetLoginDetails(this.profileForm.controls['username'].value,this.profileForm.controls['password'].value).subscribe(data=>{
+      if(data!=null){
+        if(data.userName == 'ram' && data.password =='ram'){
+          this.router.navigate(['/user']);
+         }
+      }
+     })
+
    }
   ngOnInit() {
   }
